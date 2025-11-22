186ffef3c7481934c7b584e0e053878fdd1d3fff
Jason Tan <jasontan056@gmail.com>
2025-11-21 21:45:02 -1000

    Get rid of stale js files

--- packages/api/src/router/index.js
+++
@@ -1,7 +0,0 @@
-import { router } from '../trpc.js';
-import { authRouter } from './auth.js';
-import { todoRouter } from './todo.js';
-export const appRouter = router({
-    auth: authRouter,
-    todo: todoRouter
-});
